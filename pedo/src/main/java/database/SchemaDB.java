b830ac01a6d5667216a052ffb02784d801ffcf72
valentino061117 <valentino061117@gmail.com>
2025-10-01 00:25:34 +0200

    Delete pedo directory

--- pedo/src/main/java/database/SchemaDB.java
+++
@@ -1,9 +0,0 @@
-package database;
-
-public interface SchemaDB {
-    String DB_NAME = "pasajeros";
-    String TAB_USER = "pasajeros";
-    String COL_NAME = "nombre";
-    String COL_DNI = "dni";
-    String COL_SALARIO = "salario";
-}
