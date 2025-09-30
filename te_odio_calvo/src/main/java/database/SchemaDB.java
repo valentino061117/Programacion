eafaf68629efc973d66e069951f613a3c15fc0f5
valentino061117 <valentino061117@gmail.com>
2025-09-30 23:42:50 +0200

    Delete te_odio_calvo directory

--- te_odio_calvo/src/main/java/database/SchemaDB.java
+++
@@ -1,9 +0,0 @@
-package database;
-
-public interface SchemaDB {
-    String DB_NAME = "profesores";
-    String TAB_USER ="profesores";
-    String COL_NAME = "nombre";
-    String COL_DNI = "dni";
-    String COL_SALARIO = "salario";
-}
